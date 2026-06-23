872c01c0ea92e62739c175896853cc50b3b462f7
yuva kishore <yuvakishorekolli@gmail.com>
2026-06-23 23:07:32 +0530

    updated url

--- src/app/utils.tsx
+++
@@ -1,6 +0,0 @@
-// Use Vite environment variable `VITE_BACKEND_URL` in the browser build.
-
-import  dotenv from 'dotenv';
-dotenv.config();
-
-export const URL = process.env.BACKEND_URL || "http://localhost:8000";
